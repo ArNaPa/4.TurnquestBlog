caa05307878edfa596f80b5b8324f450b88815b4
ArNaPa <130819942+ArNaPa@users.noreply.github.com>
2023-10-22 23:13:04 -0400

    Co-authored-by: ViCaDeLi <ViCaDeLi@users.noreply.github.com>

--- turnquestblog/turnquestblog/settings.py
+++ turnquestblog/turnquestblog/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'conversor.apps.ConversorConfig',            #Permite a Django Saber que existe esta aplicacion
 ]
 
 MIDDLEWARE = [
